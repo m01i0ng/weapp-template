8fe3a2c176203b99b4a1d8ec3dd292cdc50e8d87
m01i0ng <maliang1066@gmail.com>
2024-09-08 18:08:31 +0800

    feat: init template

---
+++ src/pages/chart/index.tsx
@@ -0,0 +1,80 @@
+import Taro, { setNavigationBarTitle } from '@tarojs/taro';
+import * as echarts from 'echarts/core';
+import {
+  BarChart,
+} from 'echarts/charts';
+import {
+  GridComponent
+} from 'echarts/components';
+import { Echarts, EchartsRenderer } from 'taro-charts'
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+echarts.use([
+  GridComponent,
+  EchartsRenderer,
+  BarChart,
+])
+
+const { windowWidth } = Taro.getSystemInfoSync()
+const E_HEIGHT = 300;
+const E_WIDTH = windowWidth;
+
+export default function LoupanView() {
+  useEffect(() => {
+    setNavigationBarTitle({ title: '基础柱状图' });
+  }, []);
+
+  const option = useMemo(() => {
+    return {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'bar'
+        }
+      ]
+    }
+  }, [])
+  const [chart, setChart] = useState<echarts.ECharts>();
+
+  useEffect(() => {
+    clickedCharts()
+    return () => {
+      if (process.env.TARO_ENV !== 'weapp') {
+        chart?.dispose()
+      }
+    }
+  }, [chart])
+
+  const clickedCharts = useCallback(() => {
+    chart?.on('click', function (params) {
+      console.log(params)
+    });
+  }, [chart])
+
+  return <Echarts
+    // 只有RN端需要指定RNRenderType的类型('skia'|'svg')
+    // Please specify the RNRenderType('skia'|'svg'), when you use ReactNative
+    RNRenderType='skia'
+    // 如果要渲染多个图表，需要指定不同的canvasId
+    // Please specify different canvasId, when you want to use multiple charts,
+    canvasId='chart'
+    onContextCreate={(canvas) => {
+      const chart = echarts.init(canvas, 'light', {
+        renderer: 'svg',
+        devicePixelRatio: Taro.getSystemInfoSync().pixelRatio, // 可以解决小程序下图表模糊的问题
+        width: E_WIDTH,
+        height: E_HEIGHT,
+      });
+      canvas.setChart?.(chart);
+      chart.setOption(option);
+      setChart(chart)
+    }}
+  />;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./public/index.html', './src/**/*.{html,js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
